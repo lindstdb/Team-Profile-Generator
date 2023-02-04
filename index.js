e742c6fdd2e733864343ca23557d022000ede54e
lindstdb <115056565+lindstdb@users.noreply.github.com>
2023-02-04 12:26:53 -0600

    adds git ignore, index.js w/some js, Employee class, folder structures, and .json

---
+++ index.js
@@ -0,0 +1,30 @@
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const Engineer = require("./lib/engineer");
+const Manager = require("./lib/manager");
+const Intern = require("./lib/intern");
+
+function createTeam () {
+    inquirer.createPromptModule([{
+        type: "list",
+        message: "What type of employee are you adding?",
+        name: "addEmployeePrompt",
+        choices: ["Manager", "Engineer", "Intern", "I'm done adding employees."]
+    }]).then(function(userInput) {
+        switch(userInput.addEmployeePrompt) {
+            case "Manager":
+                addManager();
+                break;
+            case "Engineer":
+                addEngineer();
+                break;
+            case "Intern":
+                addIntern();
+                break;
+
+            default:
+                htmlBuilder();
+        }
+    })
+}
