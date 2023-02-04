4a183d9fdf4dcbfd65ac6ce1646ab1394ff3caf5
lindstdb <115056565+lindstdb@users.noreply.github.com>
2023-02-04 12:48:41 -0600

    adds Manager constrctor class and manager input to index.js

--- index.js
+++ index.js
@@ -5,26 +5,61 @@ const Engineer = require("./lib/engineer");
 const Manager = require("./lib/manager");
 const Intern = require("./lib/intern");
 
-function createTeam () {
-    inquirer.createPromptModule([{
-        type: "list",
-        message: "What type of employee are you adding?",
-        name: "addEmployeePrompt",
-        choices: ["Manager", "Engineer", "Intern", "I'm done adding employees."]
-    }]).then(function(userInput) {
-        switch(userInput.addEmployeePrompt) {
-            case "Manager":
-                addManager();
-                break;
-            case "Engineer":
-                addEngineer();
-                break;
-            case "Intern":
-                addIntern();
-                break;
+teamArray = [];
 
-            default:
-                htmlBuilder();
-        }
-    })
-}
+function StartUp()
+    function createTeam () {
+        inquirer.createPromptModule([{
+            type: "list",
+            message: "What type of employee are you adding?",
+            name: "addEmployeePrompt",
+            choices: ["Manager", "Engineer", "Intern", "I'm done adding employees."]
+        }]).then(function(userInput) {
+            switch(userInput.addEmployeePrompt) {
+                case "Manager":
+                    addManager();
+                    break;
+                case "Engineer":
+                    addEngineer();
+                    break;
+                case "Intern":
+                    addIntern();
+                    break;
+
+                default:
+                    htmlBuilder();
+            }
+        })
+    }
+
+    function addManager(){
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "Enter manager's name."
+            },
+
+            {
+                type: "input",
+                name: "managerId",
+                message: "Enter the manager's employee ID number."
+            },
+
+            {
+                type:"input",
+                name: "managerEmail",
+                message: "Enter the manager's email."
+            },
+
+            {
+                type: "input",
+                name: "managerOfficeNumber",
+                message: "Enter managers office number."
+            }
+        ]).then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+            teamArray.push(manager);
+            createTeam(); 
+        });
+    }
